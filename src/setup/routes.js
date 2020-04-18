951e90028a8ba4b2b3e5051b5a97595b91691b9b
Jelle van Snik <jellevs@gmail.com>
2020-04-19 00:51:45 +0200

    Updated routes

--- src/setup/routes.js
+++ src/setup/routes.js
@@ -1,10 +1,14 @@
 const adminRouter = require("../routes/admin");
 const userRouter = require("../routes/user");
+const userRouter = require("../routes/guild");
+const webhookRouter = require("../routes/webhook");
 const { send404 } = require("../helpers/utils");
 
 function setupRoutes(app) {
   app.use("/", adminRouter);
   app.use("/user/:userid", userRouter);
+  app.use("/guild/:guildid", guildRouter);
+  app.use("/", webhookRouter);
   app.use((req, res, next) => {
     send404(res);
   });
